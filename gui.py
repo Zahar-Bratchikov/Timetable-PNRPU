c8d7f8e015eca2b4d014c7bafd8d32818a404c49
Глеб Кот <116066796+memedea1er@users.noreply.github.com>
2024-04-23 21:11:09 +0500

    Update gui.py
    убрал вторую таблицу

--- gui.py
+++ gui.py
@@ -24,20 +24,7 @@ class Ui_MainWindow(object):
                                        QtWidgets.QSizePolicy.Expanding)  # Политика изменения размеров
 
 
-        self.gridLayout.addWidget(self.tableWidget, 2, 0, 1, 1)
-
-        # Таблица 2
-        self.tableWidget_2 = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget_2.setObjectName("tableWidget_2")
-        self.tableWidget_2.setColumnCount(1)
-        self.tableWidget_2.setRowCount(6)
-        self.tableWidget_2.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        self.tableWidget_2.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
-        self.tableWidget_2.horizontalHeader().setStretchLastSection(True)  # Растягиваем последний столбец
-        self.tableWidget_2.setSizePolicy(QtWidgets.QSizePolicy.Expanding,
-                                         QtWidgets.QSizePolicy.Expanding)  # Политика изменения размеров
-
-        self.gridLayout.addWidget(self.tableWidget_2, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.tableWidget, 2, 0, 1, 2)
 
         # Кнопки и строка ввода
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
@@ -99,15 +86,6 @@ class Ui_MainWindow(object):
             "16:40": "Данные 16\nДанные 17\nДанные 18"
         }
 
-        data_week_2 = {
-            "8:00": "Данные 19\nДанные 20\nДанные 21",
-            "9:40": "Данные 22\nДанные 23\nДанные 24",
-            "11:30": "Данные 25\nДанные 26\nДанные 27",
-            "13:20": "Данные 28\nДанные 29\nДанные 30",
-            "15:00": "Данные 31\nДанные 32\nДанные 33",
-            "16:40": "Данные 34\nДанные 35\nДанные 36"
-        }
-
         # Заполнение таблицы 1
         self.tableWidget.setHorizontalHeaderLabels(["Первая неделя"])
         self.tableWidget.setVerticalHeaderLabels(["8:00", "9:40", "11:30", "13:20", "15:00", "16:40"])
@@ -116,13 +94,6 @@ class Ui_MainWindow(object):
             text_edit.setPlainText(data)
             self.tableWidget.setCellWidget(row, 0, text_edit)
 
-        # Заполнение таблицы 2
-        self.tableWidget_2.setHorizontalHeaderLabels(["Вторая неделя"])
-        self.tableWidget_2.setVerticalHeaderLabels(["8:00", "9:40", "11:30", "13:20", "15:00", "16:40"])
-        for row, (time, data) in enumerate(data_week_2.items()):
-            text_edit = QtWidgets.QTextEdit()
-            text_edit.setPlainText(data)
-            self.tableWidget_2.setCellWidget(row, 0, text_edit)
 
     # Обработчик события изменения размера окна
     def resizeEvent(self, event):
@@ -132,12 +103,6 @@ class Ui_MainWindow(object):
         row_height = int(table_height / row_count-5)
         for row in range(row_count):
             self.tableWidget.setRowHeight(row, row_height)
-
-        table_height = self.tableWidget_2.height()
-        row_count = self.tableWidget_2.rowCount()
-        row_height = int(table_height / row_count-5)
-        for row in range(row_count):
-            self.tableWidget_2.setRowHeight(row, row_height)
 
 
 import sys
