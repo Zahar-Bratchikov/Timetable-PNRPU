f871a7abce3f8bb6598995ce0f8935c9c97fbb9b
Захар Братчиков <116536086+Zahar-Bratchikov@users.noreply.github.com>
2024-05-10 20:50:53 +0500

    Add files via upload

--- parser.py
+++ parser.py
@@ -2,17 +2,17 @@ import requests
 import os
 
 
-def download_excel_schedule(faculty, group, period):
+def download_excel_schedule(Faculty, Group, Year, Number_of_group, Season, time):
     # Сформировать URL на основе предоставленных параметров
     base_url = "https://pstu.ru/files/file/Abitur/timetable/"
-    url = f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20{faculty}%20{group}%20({period}).xlsx"
+    url = f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20{Faculty}%20{Group}%20-{Year}-{Number_of_group}%20%28{Season}%20%20{time}%20smeny%29.xlsx"
 
     # Отправить GET-запрос для загрузки файла
     response = requests.get(url)
 
     if response.status_code == 200:
         # Определить путь для сохранения файла
-        filename = f"{faculty}_{group}_{period}.xlsx"
+        filename = f"{Faculty}_{Group}-{Year}-{Number_of_group}.xlsx"
         filepath = os.path.join(os.getcwd(), filename)
 
         # Записать содержимое файла
@@ -27,8 +27,20 @@ def download_excel_schedule(faculty, group, period):
 
 
 # Пример использования функции
-faculty = "AKF"
-group = "AD%20%20-21-1s"
-period = "vesennijj%20%20posle%20smeny"
 
-file_path = download_excel_schedule(faculty, group, period)
+#https://pstu.ru/files/file/Abitur/timetable/2023-2024%20Raspisanie%20zanyatijj%20{Faculty}%20{Group}%20-{Year}-{Number_of_group }%20%28{Season}%20%20{time}%20smeny%29.xlsx
+Faculty = "FPMM"
+Group = "IST"
+Year = "21"
+Number_of_group = "2b"
+Season = "osennijj"
+time = "posle"
+
+file_path = download_excel_schedule(Faculty, Group, Year, Number_of_group, Season, time)
+if file_path:
+    # Теперь можно прочитать файл Excel и обработать его с помощью pandas или openpyxl
+    # Например, можно использовать pandas:
+    """import pandas as pd
+
+    df = pd.read_excel(file_path)
+    print(df.head())  # Вывести первые несколько строк расписания"""
