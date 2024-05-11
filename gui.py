5aab8d35aaf7c7e63440401e6f174e8b83b74020
Глеб Кот <116066796+memedea1er@users.noreply.github.com>
2024-05-11 17:37:54 +0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,57 @@
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QCalendarWidget
-from PyQt5.QtCore import QTimer
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QCalendarWidget, QComboBox, QDialog, QLineEdit, QPushButton
+from timetable import Timetable
+import requests
+import os
+
+# Словари для перевода русских названий в английские эквиваленты
+GROUP_TRANSLATIONS = {
+    "БМ": "BM",
+    "ВМ": "VM",
+    "ДПМ": "DPM",
+    "ИВК": "IVK",
+    "ИСТ": "IST",
+    "ИТСИ": "ITSI",
+    "МАК": "MAK",
+    "МИЭ": "MIEH",
+    "ММ": "MM",
+    "МТВО": "MTVO",
+    "ПМ": "PM",
+    "ПМОН": "PMON",
+    "ФОП": "FOP",
+    "ХЕБИ": "KHEBI",
+    "ЦТУ": "CTU"
+}
+
+EDUCATION_LEVEL_TRANSLATIONS = {
+    "Бакалавриат": "b",
+    "Магистратура": "m",
+}
+
+SEMESTER_TRANSLATIONS = {
+    "Осенний": "osennijj",
+    "Весенний": "vesennijj",
+}
+
+PERIOD_TRANSLATIONS = {
+    "До смены": "do",
+    "После смены": "posle",
+}
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.timetable = Timetable('timetable.xlsx', 'Лист1')
+        self.ui.fill_table_data(self.timetable)
 
 
 class Ui_MainWindow(object):
+    def __init__(self):
+        self.current_day = 0
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1280, 720)
@@ -73,6 +121,7 @@ class Ui_MainWindow(object):
         self.pushButton_2.clicked.connect(self.previous_day)
         self.calendar = False
         self.pushButton_4.clicked.connect(self.show_calendar)
+        self.pushButton_3.clicked.connect(self.show_group_dialog)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -152,4 +201,92 @@ class Ui_MainWindow(object):
     def show_calendar(self):
         self.calendar = not self.calendar
         self.calendarWidget.setVisible(self.calendar)
-        QTimer.singleShot(0, lambda: self.resizeEvent(None))
+        QtCore.QTimer.singleShot(0, lambda: self.resizeEvent(None))
+
+    def show_group_dialog(self):
+        dialog = GroupSelectionDialog(self.centralwidget.window())
+        if dialog.exec_():
+            group_data = dialog.get_group_data()
+            if group_data:
+                Group, Year, Number_of_group, Level_education, Season, time = group_data
+                filename = download_excel_schedule(Group, Year, Number_of_group, Level_education, Season, time)
+                if filename:
+                    timetable = Timetable(filename, 'Лист1')
+                    self.fill_table_data(timetable)
+
+
+def download_excel_schedule(Group, Year, Number_of_group, Level_education, Season, time):
+    # Сформировать URL на основе предоставленных параметров
+    base_url = "https://pstu.ru/files/file/Abitur/timetable/"
+    url = f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20FPMM%20{Group}%20-{Year}-{Number_of_group}{Level_education}%20%28{Season}%20%20{time}%20smeny%29.xlsx"
+
+    # Отправить GET-запрос для загрузки файла
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Определить путь для сохранения файла
+        filename = f"timetable.xlsx"
+        filepath = os.path.join(os.getcwd(), filename)
+
+        # Записать содержимое файла
+        with open(filepath, "wb") as f:
+            f.write(response.content)
+
+        print(f"Файл {filename} успешно загружен.")
+        return filepath
+    else:
+        print("Не удалось загрузить файл.")
+        return None
+
+class GroupSelectionDialog(QDialog):
+    def __init__(self, parent=None):
+        super(GroupSelectionDialog, self).__init__(parent)
+        self.setWindowTitle("Выбор группы")
+        self.layout = QtWidgets.QVBoxLayout(self)
+
+        # Добавление выпадающего списка с группами
+        self.group_combo = QComboBox(self)
+        self.group_combo.addItems(GROUP_TRANSLATIONS.keys())
+        self.layout.addWidget(self.group_combo)
+
+        # Добавление элементов для ввода дополнительной информации
+        self.year_line_edit = QLineEdit(self)
+        self.year_line_edit.setPlaceholderText("Год поступления")
+        self.layout.addWidget(self.year_line_edit)
+
+        self.number_line_edit = QLineEdit(self)
+        self.number_line_edit.setPlaceholderText("Номер группы")
+        self.layout.addWidget(self.number_line_edit)
+
+        self.level_combo = QComboBox(self)
+        self.level_combo.addItems(EDUCATION_LEVEL_TRANSLATIONS.keys())
+        self.layout.addWidget(self.level_combo)
+
+        self.semester_combo = QComboBox(self)
+        self.semester_combo.addItems(SEMESTER_TRANSLATIONS.keys())
+        self.layout.addWidget(self.semester_combo)
+
+        self.period_combo = QComboBox(self)
+        self.period_combo.addItems(PERIOD_TRANSLATIONS.keys())
+        self.layout.addWidget(self.period_combo)
+
+        self.ok_button = QPushButton("ОК", self)
+        self.ok_button.clicked.connect(self.accept)
+        self.layout.addWidget(self.ok_button)
+
+    def get_group_data(self):
+        russian_group = self.group_combo.currentText()
+        english_group = GROUP_TRANSLATIONS.get(russian_group)
+        year = self.year_line_edit.text()
+        number = self.number_line_edit.text()
+        russian_level = self.level_combo.currentText()
+        english_level = EDUCATION_LEVEL_TRANSLATIONS.get(russian_level)
+        russian_semester = self.semester_combo.currentText()
+        english_semester = SEMESTER_TRANSLATIONS.get(russian_semester)
+        russian_period = self.period_combo.currentText()
+        english_period = PERIOD_TRANSLATIONS.get(russian_period)
+
+        if year and number and english_group and english_level and english_semester and english_period:
+            return english_group, year, number, english_level, english_semester, english_period
+        else:
+            return None
