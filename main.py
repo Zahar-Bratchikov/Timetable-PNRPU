8826da7850f7c4a2119bd4c6a04ffd487b6543e4
Глеб Кот <116066796+memedea1er@users.noreply.github.com>
2024-05-11 17:38:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 from PyQt5 import QtWidgets
 from timetable import Timetable
-from gui import Ui_MainWindow
-
-
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-        self.timetable = Timetable('test.xlsx', 'Лист1')
-        self.ui.fill_table_data(self.timetable)
-
+from gui import Ui_MainWindow, MainWindow
 
 if __name__ == "__main__":
     import sys
