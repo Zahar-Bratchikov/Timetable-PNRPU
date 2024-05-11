214070a5555f297cb797f930777189e385a15496
Глеб Кот <116066796+memedea1er@users.noreply.github.com>
2024-05-11 17:38:18 +0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -5,14 +5,14 @@ import os
 def download_excel_schedule(Faculty, Group, Year, Number_of_group, Season, time):
     # Сформировать URL на основе предоставленных параметров
     base_url = "https://pstu.ru/files/file/Abitur/timetable/"
-    url = f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20{Faculty}%20{Group}%20-{Year}-{Number_of_group}%20%28{Season}%20%20{time}%20smeny%29.xlsx"
+    url = f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20FPMM%20{Group}%20-{Year}-{Number_of_group}%20%28{Season}%20%20{time}%20smeny%29.xlsx"
 
     # Отправить GET-запрос для загрузки файла
     response = requests.get(url)
 
     if response.status_code == 200:
         # Определить путь для сохранения файла
-        filename = f"{Faculty}_{Group}-{Year}-{Number_of_group}.xlsx"
+        filename = f"test1.xlsx"
         filepath = os.path.join(os.getcwd(), filename)
 
         # Записать содержимое файла
@@ -28,7 +28,7 @@ def download_excel_schedule(Faculty, Group, Year, Number_of_group, Season, time)
 
 # Пример использования функции
 
-#https://pstu.ru/files/file/Abitur/timetable/2023-2024%20Raspisanie%20zanyatijj%20{Faculty}%20{Group}%20-{Year}-{Number_of_group }%20%28{Season}%20%20{time}%20smeny%29.xlsx
+# https://pstu.ru/files/file/Abitur/timetable/2023-2024%20Raspisanie%20zanyatijj%20{Faculty}%20{Group}%20-{Year}-{Number_of_group }%20%28{Season}%20%20{time}%20smeny%29.xlsx
 Faculty = "FPMM"
 Group = "IST"
 Year = "21"
