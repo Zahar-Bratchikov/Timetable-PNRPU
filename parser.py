651c5fdc0c364c5f5eaabebe1239d74407ea37ba
Язов Михаил <100093644+MRxPosiTiVe@users.noreply.github.com>
2024-05-13 20:32:24 +0500

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,16 +1,31 @@
 import os
+from calendar import Month
+from datetime import datetime
+
 import requests
 from PyQt5.QtWidgets import QMessageBox
 
 
 def download_excel_schedule(Group, Year, Number_of_group, Level_education, Season, time):
+
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    # 1 - 7 prev-now
+    # 8 - 12 now-next
+
+    if current_month in range(1, 8):
+        year_semester = f"{current_year - 1}-{current_year}"
+    else:
+        year_semester = f"{current_year}-{current_year + 1}"
+
+
     # Сформировать URL на основе предоставленных параметров
     base_url = "https://pstu.ru/files/file/Abitur/timetable/"
-    url = (f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20FPMM%20{Group}%20-"
+    url = (f"{base_url}{year_semester}%20Raspisanie%20zanyatijj%20FPMM%20{Group}%20-"
            f"{Year}-{Number_of_group}{Level_education}%20%28{Season}%20%20{time}%20smeny%29.xlsx")
-    url2 = (f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20FPMM%20{Group}%20%20-"
+    url2 = (f"{base_url}{year_semester}%20Raspisanie%20zanyatijj%20FPMM%20{Group}%20%20-"
             f"{Year}-{Number_of_group}{Level_education}%20%28{Season}%20%20{time}%20smeny%29.xlsx")
-    url3 = (f"{base_url}2023-2024%20Raspisanie%20zanyatijj%20FPMM%20{Group}-"
+    url3 = (f"{base_url}{year_semester}%20Raspisanie%20zanyatijj%20FPMM%20{Group}-"
             f"{Year}-{Number_of_group}{Level_education}%20%28{Season}%20%20{time}%20smeny%29.xlsx")
     # Отправить GET-запрос для загрузки файла
     response = requests.get(url)
